139c4de295f86105d4a8f64131e4658485d5d51c
Muiz <amuizzuddinm06k@gmail.com>
2026-08-30 22:10:25 +0800

    Push Notifications
    - add push notifications on main.dart
    - add import firebase messaging
    - add firebase messaging on pubsec.yaml

--- lib/main.dart
+++ lib/main.dart
@@ -2,17 +2,73 @@ import 'package:flutter/material.dart';
 import 'auth_wrapper.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 
+@pragma('vm:entry-point')
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  print('Handling a background message: ${message.messageId}');
+}
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+// Safely requests permissions and gets token after the UI mounts
+void _setupPushNotifications() async {
+  FirebaseMessaging messaging = FirebaseMessaging.instance;
+
+  // Request notification permissions
+  NotificationSettings settings = await messaging.requestPermission(
+    alert: true,
+    badge: true,
+    sound: true,
+  );
+
+  if (settings.authorizationStatus == AuthorizationStatus.authorized) {
+    print('User granted notification permission');
+    
+    // Get the FCM Token
+    String? token = await messaging.getToken();
+    print("FCM Token: $token");
+
+    // Handle messages while the app is active and open on the screen
+    FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+      print('Got a message whilst in the foreground!');
+      
+      // FIX: Extracting to a local final variable forces type promotion to bypass the analyzer error
+      final notification = message.notification;
+      
+      if (notification != null) {
+        print('Notification Title: ${notification.title}');
+        print('Notification Body: ${notification.body}');
+      }
+    });
+  } else {
+    print('User declined notification permission');
+  }
+}
+
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+} 
+class _MyAppState extends State<MyApp> {
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      _setupPushNotifications();
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
